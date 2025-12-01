a1ef2c4a9fd9fdb84ceaa564ed5eadd9792809bd
Sabbir Ahamed <2110737@iub.edu.bd>
2025-12-01 22:14:12 +0600

    Summary is done

---
+++ uni_assignment/lib/ui/summary.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/common_widgets.dart';
+
+class PageSummary extends StatelessWidget {
+  const PageSummary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonPage(title: "Summary");
+  }
+}
