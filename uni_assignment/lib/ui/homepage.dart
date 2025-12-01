a079a53d1793533736f4ec7902d634d3f58b23ac
Sabbir Ahamed <2110737@iub.edu.bd>
2025-12-01 22:12:44 +0600

    done homepage

---
+++ uni_assignment/lib/ui/homepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/common_widgets.dart';
+
+class PageHome extends StatelessWidget {
+  const PageHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonPage(title: "Home");
+  }
+}
