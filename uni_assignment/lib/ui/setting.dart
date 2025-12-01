f805f341e4ca0e0e1f3275d5fe6ff2f860994c2d
Sabbir Ahamed <2110737@iub.edu.bd>
2025-12-01 22:15:16 +0600

    Setting page is done

---
+++ uni_assignment/lib/ui/setting.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uni_assignment/widgets/common_widgets.dart';
+
+class PageSetting extends StatelessWidget {
+  const PageSetting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CommonPage(title: "Settings");
+  }
+}
